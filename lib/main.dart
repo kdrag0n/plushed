94e28dbae4bad62c9cf938b196d52cb662d50ea4
Danny Lin <danny@kdrag0n.dev>
2020-01-05 19:00:24 -0800

    lib: Split camera code out of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 
+import 'camera.dart';
 
 void main() => runApp(PlushedApp());
 
@@ -16,56 +16,6 @@ class PlushedApp extends StatelessWidget {
         backgroundColor: Colors.black,
       ),
       home: CameraPage(title: 'Plushed'),
-    );
-  }
-}
-
-class CameraPage extends StatefulWidget {
-  CameraPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _CameraPageState createState() => _CameraPageState();
-}
-
-class _CameraPageState extends State<CameraPage> {
-  CameraController _controller;
-  Future<void> _initCameraFuture;
-
-  Future<void> initCamera() async {
-    List<CameraDescription> cameras = await availableCameras();
-    _controller = CameraController(cameras.first, ResolutionPreset.veryHigh);
-    await _controller.initialize();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _initCameraFuture = initCamera();
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<void>(
-        future: _initCameraFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
     );
   }
 }
